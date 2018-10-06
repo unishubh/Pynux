3e7222cfd619f482466df50b5a49afcbe8dc4a99
Quinn Perfetto <thequinnftw@gmail.com>
2018-10-06 10:46:26 -0700

    added tab based autocompletion of commands and the directory contents

--- index.py
+++ index.py
@@ -1,5 +1,18 @@
 from terminal import Terminal
 import readline
+import commands
+import os
+
+valid_commands = [command for command in commands.__dict__.keys()
+                          if not command.startswith("__")]
+
+# Given the query text and the current state, attempt to provide
+# an autocompletion. This function will use the contents of the current
+# directory as well as all valid commands as the possible results.
+def completer(text, state):
+    complete_corpus = valid_commands + os.listdir(".")
+    options = [option for option in complete_corpus if option.startswith(text)]
+    return options[state] if state < len(options) else None
 
 def get_command():
     command = raw_input()
@@ -8,5 +21,10 @@ def get_command():
 
 if __name__ == "__main__":
     print "Welcome to pylinux"
+
+    readline.set_completer(completer)
+    readline.parse_and_bind("tab: complete")
+    readline.parse_and_bind("bind ^I rl_complete")
+
     terminal = Terminal()
     terminal.play()
